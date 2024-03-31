db91881dc76af65fe6350cf9e740c3032e41c30f
Timothy M <timmykmcc@gmail.com>
2024-03-31 18:14:10 -0400

    Connected Engine to User Data
    Ratings added through engine will be added to user ratings data. When you run it currently it will add the ratings to the mockUserData

--- application/recommendationEngine.js
+++ application/recommendationEngine.js
@@ -3,17 +3,31 @@
 //Group Members: Crystal Byrd, Tim McCarty, Ikaia Melton, Michael Minnick
 //Engine works by checking similarity between users and creating a recommendation score based on the similarity level to other users
 //Similarity is checked by how close ratings were to the same items. Same ratings gives very similar and a rating one point away is just similar. Anything further is valued as not similar.
+const fs = require('fs');
+const userData = require('./data/mockUserData.json');
+
+
 class RecommendationEngine {
     constructor() {
         this.userRatings = new Map();
     }
 
-    addRating(user, movie, rating) {
+    addRating(user, movieId, rating) {
         if (!this.userRatings.has(user)) {
             this.userRatings.set(user, new Map());
         }
-        this.userRatings.get(user).set(movie, rating);
+        this.userRatings.get(user).set(movieId, rating);
+    
+        // Update user data in mockUserData.json
+        const userIndex = userData.findIndex(userData => userData.username === user);
+        if (userIndex !== -1) {
+            userData[userIndex].ratings.push({ "movie_id": movieId, "rating": rating });
+            fs.writeFileSync('./application/data/mockUserData.json', JSON.stringify(userData, null, 2));
+        } else {
+            console.log(`User ${user} not found in mockUserData.json.`);
+        }
     }
+    
 
     getRecommendations(user) {
         const recommendations = new Map();
@@ -74,61 +88,25 @@ class RecommendationEngine {
         return similarity;
     }
 
-    addTestData(){
-        this.addRating("User1", "Movie1", 5.0);
-        this.addRating("User1", "Movie2", 4.0);
-        this.addRating("User1", "Movie3", 3.0);
-        this.addRating("User1", "Movie4", 2.0);
-
-        this.addRating("User2", "Movie2", 5.0);
-        this.addRating("User2", "Movie3", 4.0);
-        this.addRating("User2", "Movie4", 3.0);
-        this.addRating("User2", "Movie5", 2.0);
-
-        this.addRating("User3", "Movie3", 5.0);
-        this.addRating("User3", "Movie4", 4.0);
-        this.addRating("User3", "Movie5", 3.0);
-        this.addRating("User3", "Movie8", 5.0);
-
-        this.addRating("User4", "Movie4", 5.0);
-        this.addRating("User4", "Movie5", 4.0);
-        this.addRating("User4", "Movie6", 3.0);
-        this.addRating("User4", "Movie8", 3.0);
-
-        this.addRating("User5", "Movie1", 5.0);
-        this.addRating("User5", "Movie5", 5.0);
-        this.addRating("User5", "Movie6", 4.0);
-        this.addRating("User5", "Movie7", 3.0);
-    }
 }
 
 
 const capStoneEngine = new RecommendationEngine();
 
-capStoneEngine.addRating("User1", "Movie1", 5.0);
-capStoneEngine.addRating("User1", "Movie2", 4.0);
-capStoneEngine.addRating("User1", "Movie3", 3.0);
-capStoneEngine.addRating("User1", "Movie4", 2.0);
+capStoneEngine.addRating("FakeUser_Horror", 101, 5.0);
+capStoneEngine.addRating("FakeUser_Horror", 102, 4.5);
+capStoneEngine.addRating("FakeUser_Horror", 103, 4.0);
 
-capStoneEngine.addRating("User2", "Movie2", 5.0);
-capStoneEngine.addRating("User2", "Movie3", 4.0);
-capStoneEngine.addRating("User2", "Movie4", 3.0);
-capStoneEngine.addRating("User2", "Movie5", 2.0);
+capStoneEngine.addRating("FakeUser_Action", 102, 5.0);
+capStoneEngine.addRating("FakeUser_Action", 103, 4.5);
+capStoneEngine.addRating("FakeUser_Action", 104, 4.0);
 
-capStoneEngine.addRating("User3", "Movie3", 5.0);
-capStoneEngine.addRating("User3", "Movie4", 4.0);
-capStoneEngine.addRating("User3", "Movie5", 3.0);
-capStoneEngine.addRating("User3", "Movie8", 5.0);
+capStoneEngine.addRating("FakeUser_Comedy", 103, 5.0);
+capStoneEngine.addRating("FakeUser_Comedy", 104, 4.5);
+capStoneEngine.addRating("FakeUser_Comedy", 105, 4.0);
 
-capStoneEngine.addRating("User4", "Movie4", 5.0);
-capStoneEngine.addRating("User4", "Movie5", 4.0);
-capStoneEngine.addRating("User4", "Movie6", 3.0);
-capStoneEngine.addRating("User4", "Movie8", 3.0);
 
-capStoneEngine.addRating("User5", "Movie1", 5.0);
-capStoneEngine.addRating("User5", "Movie5", 5.0);
-capStoneEngine.addRating("User5", "Movie6", 4.0);
-capStoneEngine.addRating("User5", "Movie7", 3.0);
+//Add a loop for each genre that creates a user that gives each specific genre a 5 rating.
 
 for (const user of capStoneEngine.userRatings.keys()) {
     const userRecommendations = capStoneEngine.getRecommendations(user);
@@ -139,7 +117,6 @@ for (const user of capStoneEngine.userRatings.keys()) {
 }
 
 console.log("This is an example of how to call for all of the recommendations and their associated scores for one user: ");
-console.log(capStoneEngine.getAllRecommendations("User5"));
-
+console.log(capStoneEngine.getAllRecommendations("FakeUser_Comedy"));
 
 module.exports = { RecommendationEngine };
